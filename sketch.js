cdab3417b7ee8456e4e363214485069f5f0e613c
Marianna <marianna-vertinskaya@mail.ru>
2020-04-28 10:47:14 +0300

    menu done

--- sketch.js
+++ sketch.js
@@ -1,3 +1,13 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  }else{
+    x.className = "menu";
+  }
+}
+
+
 var p = [];
 var n = 20;
 
